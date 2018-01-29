e4799a3597b2de1d7dabac77235cb52a63c5092f
Greg Macfarlane <gregmacfarlane@gmail.com>
2018-01-29 11:11:09 -0500

    Add function to compute cultural and environmental impact

---
+++ R/sensitive_uses.R
@@ -0,0 +1,79 @@
+#' Compute cultural and environmental resources measure
+#'
+#' @details
+#' The cultural and environmental resources measure is the share of a project's
+#'  alignment lying within sensitive land uses. These land uses are defined by
+#'  areal, linear, or point geometries; in the case of points and lines, a buffer
+#'  is applied to the geometries.
+#'
+#' The buffer radius calculation requires that the project and sensitivity layer be
+#'  in the same projected coordinate system.
+#'
+#' @param project An object of class `sf` identifing a project alignment
+#' @param sensitive_layer An object of class `sf` defining sensitive land uses.
+#' @param buffer Distance considered an impact on point and linear uses, in the
+#'   units of `epsg`
+#' @param epsg The EPSG code of the reprojected layer.
+#'
+#'
+#' @return The percent of the project's alignment lying in sensitive areas
+#'
+#'
+#' @importFrom sf st_geometry_type st_crs st_transform st_buffer st_intersection st_as_sf st_length
+#' @importFrom dplyr filter
+#'
+#'
+#' @export
+#'
+cultenv <- function(project, sensitive_layer, buffer, epsg = NULL){
+
+  # if no epsg
+  if(is.null(epsg)){
+    if(sf::st_crs(project) != sf::st_crs(sensitive_layer)){
+      stop("CRS of project and sensitive resources not the same. Please provide epsg.")
+    }
+  }
+
+
+  # re-project layers
+  project <- project %>%
+    dplyr::filter(sf::st_geometry_type(.$geometry) %in%
+                    c("MULTILINESTRING", "LINESTRING")) %>%
+    sf::st_transform(epsg)
+
+  sensitive_layer <- sensitive_layer %>%
+    sf::st_transform(epsg)
+
+
+  # buffer points and lines
+  point_line_types <- c("MULTILINESTRING", "LINESTRING", "POINT", "MULTIPOINT")
+  lines_points <- sensitive_layer %>%
+    dplyr::filter(sf::st_geometry_type(.$geometry) %in% point_line_types)
+
+  if(nrow(lines_points) > 0){
+    lines_points <- lines_points %>%
+    sf::st_as_sf(promote_to_multi = TRUE)  %>%
+    sf::st_buffer(buffer)
+  }
+
+
+  # get areas
+  area_types <- c("MULTIPOLYGON", "POLYGON")
+  areas <- sensitive_layer %>%
+    dplyr::filter(sf::st_geometry_type(.$geometry) %in% area_types)
+
+  if(nrow(areas) > 0){
+    areas <- areas %>%
+      sf::st_as_sf(promote_to_multi <- TRUE)
+  }
+
+  sens_buffs <- rbind(areas, lines_points)
+
+  # intersect project with sensitivity buffers
+  intersected <- project %>%
+    sf::st_intersection(sens_buffs)
+
+  # calculate lengths and divide
+  as.numeric(sf::st_length(intersected) / sf::st_length(project))
+
+}
