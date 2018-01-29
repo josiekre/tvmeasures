ce54da61d26f763adf1df61e034353321ca073dc
Greg Macfarlane <gregmacfarlane@gmail.com>
2018-01-29 11:09:24 -0500

    Use generic pop and jobs in access management
    As opposed to the difference in system jobs with and without the project

--- R/asset_mgmt.R
+++ R/asset_mgmt.R
@@ -1,8 +1,8 @@
-#' Compute the Asset Management Statistic
+#' Compute the Asset Management measure
 #'
-#' @param new_pop The population within 1/2 mile of proposed transit project
+#' @param pop The population within 1/2 mile of proposed transit project
 #'   stops or stations with new access to transit.
-#' @param new_jobs The jobs within 1/2 mile of proposed transit project
+#' @param jobs The jobs within 1/2 mile of proposed transit project
 #'   stops or stations with new access to transit.
 #' @param service_miles The average weekday service miles of the proposed project.
 #'
@@ -10,8 +10,8 @@
 #'
 #' @export
 #'
-asset_mgmt <- function(new_pop, new_jobs, service_miles) {
+asset_mgmt <- function(pop, jobs, service_miles) {
 
-  (new_pop + new_jobs) / service_miles
+  (pop + jobs) / service_miles
 
 }
