b16026731e5580039683a0d7a860bf0e994f140d
Greg Macfarlane <gregmacfarlane@gmail.com>
2018-04-30 13:05:38 -0400

    Shiny application framework

---
+++ inst/tvmeasures/remix.md
@@ -0,0 +1,7 @@
+A csv file containing the results of the Remix analysis. The file should have fields with these names:
+  - `project`: The name of the project
+  - `population`: The population in the project service area
+  - `jobs`: The jobs in the project service area
+  - `service_miles`: The weekday service miles of the project
+  - `pct_minority`: The percent of population in the service area who are minority
+  - `pct_poverty`: The percent of population in the service area who have incomes under the poverty line
---
+++ inst/tvmeasures/ui.R
@@ -0,0 +1,66 @@
+#
+# This is the user-interface definition of a Shiny web application. You can
+# run the application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library(leaflet)
+
+# Define UI for application that draws a histogram
+shinyUI(fluidPage(
+
+  # Application title
+  titlePanel("Concept 3 Evaluation Measures"),
+
+  #
+  sidebarLayout(
+    sidebarPanel(
+      p("This application aids ARC staff as they compute the output from",
+        "a Concept 3 project evaluation. There are four tabs, each with",
+        "independent input files."),
+
+      # remix table with population, jobs, etc
+      h3("Remix"),
+      includeMarkdown("remix.md"),
+      #fileInput(),
+
+
+      # cultural/environmental shapefile or geojson
+      h3("Cultural / Environmental"),
+      includeMarkdown("cultenv.md"),
+      #fileInput(),
+
+
+      # conveyal rasters
+      h3("Conveyal"),
+      includeMarkdown("conveyal.md"),
+      #fileInput(),
+
+
+      # map
+      h3("Accessibility Map"),
+      includeMarkdown("raster.md")
+      #fileInput(),
+      #fileInput()
+
+
+
+    ),
+
+    # Show a plot of the generated distribution
+    mainPanel(
+
+      tabsetPanel(
+        type = "tabs",
+        tabPanel("Remix", dataTableOutput("remix_output")),
+        tabPanel("Cultural/Environmental", dataTableOutput("cultural_environmental")),
+        tabPanel("Conveyal", dataTableOutput("conveyal_output")),
+        tabPanel("Accessibility Map", leafletOutput("raster_output"))
+      )
+    )
+  )
+))
